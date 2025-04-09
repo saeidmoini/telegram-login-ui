7fe114fa0e6fb17aff91cc4125f59c3bb7200c0b
saeidmoini <saeid.moini@gmail.com>
2025-04-09 11:53:42 +0330

    Remove unwanted files and update .gitignore

---
+++ build/lib/telegram_login_ui/__init__.py
@@ -0,0 +1,2 @@
+from .app import login_bp
+from .telegram_client import TelegramClientHandler
--- setup.py
+++ setup.py
@@ -2,22 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='telegram-login-ui',
-    version='1.0.0',
+    version='1.0.1',
     description='A package to create a simple UI for logging into Telegram accounts and managing sessions.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Saeid Moini',
     author_email='saeid.moini1997@gmail.com',
     url='https://github.com/saeidmoini/telegram-login-ui',
-    include_package_data=True,
+    packages=find_packages(),  # Ensure this is included
     install_requires=[
         'Flask>=2.0.0',
         'python-dotenv>=0.21.0',
         'telethon>=1.24.0',
         'pyjwt>=2.6.0',
-        'asyncio',  # For asynchronous operations
-        'shutil',   # For file operations (part of Python standard library, no need to install)
-        'json',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
